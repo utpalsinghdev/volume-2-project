af69738f02b843ff7e79ef8abe11b59d495faaf2
Utpal Singh <utpalsingh915@gmail.com>
2023-10-22 16:02:36 +0530

    from ui done

---
+++ app/_components/Form.tsx
@@ -0,0 +1,295 @@
+"use client";
+import React from "react";
+import { Formik } from "formik";
+import { PlusCircleIcon, PlusSmallIcon } from "@heroicons/react/24/solid";
+
+const Form = () => {
+  return (
+    <Formik
+      initialValues={{
+        firstName: "",
+        lastName: "",
+        email: "",
+        country: "",
+        address: "",
+        state: "",
+        city: "",
+        pin: "",
+        acedmics_record: [
+          {
+            degree: "",
+            marks: "",
+            marksIn: "",
+          },
+        ],
+      }}
+      onSubmit={(values, action) => {}}
+    >
+      <form>
+        <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+          <div className="sm:col-span-3">
+            <label
+              htmlFor="first-name"
+              className="block text-sm font-medium leading-6 text-white"
+            >
+              First name
+            </label>
+            <div className="mt-2">
+              <input
+                type="text"
+                name="first-name"
+                id="first-name"
+                autoComplete="given-name"
+                className="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+
+          <div className="sm:col-span-3">
+            <label
+              htmlFor="last-name"
+              className="block text-sm font-medium leading-6 text-white"
+            >
+              Last name
+            </label>
+            <div className="mt-2">
+              <input
+                type="text"
+                name="last-name"
+                id="last-name"
+                autoComplete="family-name"
+                className="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+
+          <div className="sm:col-span-4">
+            <label
+              htmlFor="email"
+              className="block text-sm font-medium leading-6 text-white"
+            >
+              Email address
+            </label>
+            <div className="mt-2">
+              <input
+                id="email"
+                name="email"
+                type="email"
+                autoComplete="email"
+                className="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+
+          <div className="sm:col-span-3">
+            <label
+              htmlFor="country"
+              className="block text-sm font-medium leading-6 text-white"
+            >
+              Country
+            </label>
+            <div className="mt-2">
+              <select
+                id="country"
+                name="country"
+                autoComplete="country-name"
+                className="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6 [&_*]:text-black"
+              >
+                <option>United States</option>
+                <option>Canada</option>
+                <option>Mexico</option>
+              </select>
+            </div>
+          </div>
+
+          <div className="col-span-full">
+            <label
+              htmlFor="street-address"
+              className="block text-sm font-medium leading-6 text-white"
+            >
+              Street address
+            </label>
+            <div className="mt-2">
+              <textarea
+                rows={3}
+                name="street-address"
+                id="street-address"
+                autoComplete="street-address"
+                className="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+
+          <div className="sm:col-span-2 sm:col-start-1">
+            <label
+              htmlFor="city"
+              className="block text-sm font-medium leading-6 text-white"
+            >
+              State
+            </label>
+            <div className="mt-2">
+              <input
+                type="text"
+                name="city"
+                id="city"
+                autoComplete="address-level2"
+                className="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+
+          <div className="sm:col-span-2">
+            <label
+              htmlFor="region"
+              className="block text-sm font-medium leading-6 text-white"
+            >
+              City
+            </label>
+            <div className="mt-2">
+              <input
+                type="text"
+                name="region"
+                id="region"
+                autoComplete="address-level1"
+                className="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+
+          <div className="sm:col-span-2">
+            <label
+              htmlFor="postal-code"
+              className="block text-sm font-medium leading-6 text-white"
+            >
+              ZIP / Postal code
+            </label>
+            <div className="mt-2">
+              <input
+                type="text"
+                name="postal-code"
+                id="postal-code"
+                autoComplete="postal-code"
+                className="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+        </div>
+        <div className="border-b mt-4 border-white/10 pb-12">
+          <h2 className="text-base font-semibold leading-7 text-white">
+            Academics Information
+          </h2>
+          <p className="mt-1 text-sm leading-6 text-gray-400">
+            Use a permanent address where you can receive mail.
+          </p>
+        </div>
+        <h2 className="text-base font-semibold leading-7 mt-4  text-white">
+          Qualifications{" "}
+          <button
+            type="button"
+            className="rounded-md bg-transparent border-2 ml-2 hover:bg-indigo-500 duration-300 border-indigo-500 px-3 py-2 text-sm font-semibold text-white shadow-sm "
+          >
+            + Add more
+          </button>
+        </h2>
+        <div className="grid grid-cols-1 gap-8 mt-4">
+          <div className=" sm:col-start-1">
+            <label
+              htmlFor="city"
+              className="block text-sm font-medium leading-6 text-white"
+            >
+              degree
+            </label>
+            <div className="mt-2">
+              <input
+                type="text"
+                name="city"
+                id="city"
+                autoComplete="address-level2"
+                className="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6 [&_*]:text-black"
+              />
+            </div>
+          </div>
+          <h2 className="text-base font-semibold leading-7  text-white">
+            Subjects{" "}
+            <button
+              type="button"
+              className="rounded-md bg-transparent border-2 ml-2 hover:bg-indigo-500 duration-300 border-indigo-500 px-3 py-2 text-sm font-semibold text-white shadow-sm "
+            >
+              + Add more
+            </button>
+          </h2>
+          <div className="px-4 grid grid-cols-1 md:grid-cols-3 gap-8">
+            <div>
+              <label
+                htmlFor="city"
+                className="block text-sm font-medium leading-6 text-white"
+              >
+                Subject
+              </label>
+              <div className="mt-2">
+                <input
+                  type="text"
+                  name="city"
+                  id="city"
+                  autoComplete="address-level2"
+                  className="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <label
+                htmlFor="region"
+                className="block text-sm font-medium leading-6 text-white"
+              >
+                Obtained Marks
+              </label>
+              <div className="mt-2">
+                <input
+                  type="text"
+                  name="region"
+                  id="region"
+                  autoComplete="address-level1"
+                  className="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+            <div>
+              <label
+                htmlFor="region"
+                className="block text-sm font-medium leading-6 text-white"
+              >
+                Full Marks
+              </label>
+              <div className="mt-2">
+                <input
+                  type="text"
+                  name="region"
+                  id="region"
+                  autoComplete="address-level1"
+                  className="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="mt-6 flex items-center justify-end gap-x-6">
+          <button
+            type="button"
+            className="text-sm font-semibold leading-6 text-white"
+          >
+            Cancel
+          </button>
+          <button
+            type="submit"
+            className="rounded-md bg-indigo-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
+          >
+            Apply
+          </button>
+        </div>
+      </form>
+    </Formik>
+  );
+};
+
+export default Form;
